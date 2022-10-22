bc0edd912365b829228e0fb1f6d0d5cc0bb07155
Jonphia Hart <soldiergurl4life@gmail.com>
2022-10-22 10:57:55 -0400

    updated personal budget

--- index.js
+++ index.js
@@ -3,3 +3,40 @@ let monthlyExpenses = 0
 let annualExpenses = 0
 
 // Your Code Here
+//console.log(weeklyExpenses,monthlyExpenses,annualExpenses)
+console.log(weeklyExpenseQuestions)
+
+for(let i = 0; i< weeklyExpenseQuestions.length; i++){
+    let response = parseInt(window.prompt(weeklyExpenseQuestions[i])) 
+    console.log(typeof response)
+    weeklyExpenses = weeklyExpenses + response 
+    console.log(weeklyExpenses)
+
+
+}
+
+for(let i = 0; i< monthlyExpenseQuestions.length; i++){
+    let response = parseInt(window.prompt(monthlyExpenseQuestions[i])) 
+    console.log(typeof response)
+    weeklyExpenses = weeklyExpenses + response 
+    console.log(weeklyExpenses)
+
+
+}
+
+for(let i = 0; i< annualExpenseQuestions.length; i++){
+    let response = parseInt(window.prompt(annualExpenseQuestions[i])) 
+    console.log(typeof response)
+    weeklyExpenses = weeklyExpenses + response 
+    console.log(weeklyExpenses)
+
+
+}
+
+
+
+
+
+
+//console.log(monthlyExpenseQuestions)
+//console.log(annualExpenseQuestions)
